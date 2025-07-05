2d433271296819d49e0f9210d2092573ea881904
Lucas Mosca <88050952+L-Mosca@users.noreply.github.com>
2025-07-05 17:41:56 -0300

    feature: add home screen

---
+++ app/src/main/java/br/com/poopapp/HomeActivity.kt
@@ -0,0 +1,14 @@
+package br.com.poopapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/br/com/poopapp/SplashActivity.kt
+++ app/src/main/java/br/com/poopapp/SplashActivity.kt
@@ -1,6 +1,9 @@
 package br.com.poopapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,11 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
